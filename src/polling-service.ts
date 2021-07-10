2ef8ac839e2fae3f3f00c9c066a1210eb97ff4fd
DarrenN <kensei189@gmail.com>
2021-07-10 17:16:59 +0200

    feature: initial code commit

---
+++ src/polling-service.ts
@@ -0,0 +1,56 @@
+
+type Callback = () => any
+
+interface IPollingService {
+  start: (callback: Callback) => void;
+  stop: () => void;
+} 
+
+const POLL_INTERVAL = 2000;
+
+export class PollingService implements IPollingService{
+  private static instance: PollingService
+  private timer?: NodeJS.Timer;
+  constructor() {
+    if (!PollingService.instance) {
+      PollingService.instance = this;
+      this.timer = undefined;
+    }
+    return PollingService.instance;
+  }
+
+  public start(callback: Callback): void {
+    this.timer = setInterval(callback, POLL_INTERVAL)
+  }
+
+  public stop(): void {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+}
+
+const pollingService = new PollingService();
+export default pollingService;
+
+
+
+type Listener<EventType> = (ev: EventType) => void;
+
+function createObserver<EventType>(): {
+  subscribe: (listener: Listener<EventType>) => () => void;
+  publish: (event: EventType) => void;
+} {
+  let listeners: Listener<EventType>[] = [];
+  return {
+    subscribe: (listener: Listener<EventType>): () => void => {
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter(l => l !== listener)
+      }
+    },
+    publish: (event: EventType) => {
+      listeners.forEach((l) => l(event))
+    }
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import securityMiddleware from '@middleware/security';
+import pollingService from './polling-service';
+
+
+const app = express();
+const port = 3000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(securityMiddleware);
+
+const server = app.listen(port, () => {
+  console.log(
+    "[%s] Listening on http://localhost:%d",
+    process.env.NODE_ENV,
+    port
+  );
+})
+
+app.get('/ping', (_: Request, res: Response) => res.status(StatusCodes.OK).send('pong'));
+
+let tick = 0;
+
+const cb = () => {
+  tick++;
+  console.log(`Tick ===> ${tick}`)
+};
+
+pollingService.start(cb)
+
+process.on('SIGTERM', () => {
+  console.debug('SIGTERM signal received: closing HTTP server')
+  server.close(() => {
+    console.debug('HTTP server closed')
+  })
+})
+
